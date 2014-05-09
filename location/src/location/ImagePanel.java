92a44bd68638e4010b3f87341beea26d1c0fd1e1
1 <1@your-4c2a19a0bf>
2014-05-09 17:13:16 +0400

    разбито на отдельные функции

--- location/src/location/ImagePanel.java
+++ location/src/location/ImagePanel.java
@@ -35,6 +35,11 @@ public class ImagePanel extends JPanel {
 	/** Минимальный масштаб */
 	private int minM = 1;
 	
+	/** Радиус круга, которым отбражается базовая станция (в ячейках базовой сетки) */
+	private double rad = 0.5;
+	
+	Plan plan;
+	
 	/**
 	 * @return масштаб
 	 */
@@ -47,138 +52,179 @@ public class ImagePanel extends JPanel {
 	 * @param m1 новое значение масштаба
 	 */
 	public void setM(int m1) {
-		width += (hBars / m) * (m1 - m);
-		height += (vBars / m) * (m1 - m);
-		setMinimumSize(new Dimension(width, height));
+		width += (hBars / m) * (m1 - m) * bar;
+		height += (vBars / m) * (m1 - m) * bar;
 		setPreferredSize(new Dimension(width, height));
+		setMinimumSize(new Dimension(width, height));
 		m = m1;
+		hBars = width / bar;
+		vBars = height / bar;
+		
+		System.out.println(width + " " + height);
 	}
 
 
-    	/** 
-     	* @param width ширина поля для рисования
-     	* @param height высота поля для рисования
-     	*/
-    	ImagePanel(int w, int h, Location l) {
-			super();
+    /** 
+    * @param width ширина поля для рисования
+    * @param height высота поля для рисования
+    */
+    ImagePanel(int w, int h, Location l) {
+		super();
+		width = w;
+		height = h;
 	
-			width = w;
-			height = h;
-	
-			//задаем постоянный размер
-			Dimension size = new Dimension(width, height);
-			setMaximumSize(size);
-			setMinimumSize(size);
+		//задаем постоянный размер
+		Dimension size = new Dimension(width, height);
+		setMaximumSize(size);
+		setMinimumSize(size);
 			
-			location = l;
+		location = l;
 	
-			//рассчитываем количество ячеек, умещающихся по горизонтали и вертикали
-			hBars = width / bar;
-			vBars = height / bar;
-    	}
+		//рассчитываем количество ячеек, умещающихся по горизонтали и вертикали
+		hBars = width / bar;
+		vBars = height / bar;
+   	}
 
-    	public void update(Graphics gr) {
-    		paintComponent(gr);
-    	}
+   	public void update(Graphics gr) {
+   		paintComponent(gr);
+   	}
 
     	public void paintComponent(Graphics gr) {
         	super.paintComponent(gr);
 			Graphics2D g = (Graphics2D) gr;
-	
+			plan = location.getPlan();
+			
 			//отрисоываваем базовую сетку
-			BasicStroke b = new BasicStroke(bPen); 
-			g.setStroke(b);
-			g.setColor(Color.GRAY);
-			for (int i = 0; i < vBars; i++)
-				g.drawLine(0, bar * i, width, bar * i);
-			for (int i = 0; i < hBars; i++)
-				g.drawLine(bar * i, 0, bar * i, height);
-	
-			Plan plan = location.getPlan();
-	
-			//отрисовываем внутренние клетки
-			/*if (location.hasOpenFile()) {
-				for (int i = 0; i < location.frames.length; i++)
-					for (int j = 0; j < location.frames[0].length; j++)
-						if (!location.frames[i][j].isUsed())
-							fillColor(location.frames[i][j].getX1() * m * bar, 
-							location.frames[i][j].getY1() * m * bar,
-							location.frames[i][j].getX2() * m * bar, 
-							location.frames[i][j].getY2() * m * bar);
-			}*/
-	
+			drawBaseLines(g);
 			
 			//отрисовываем стены
-			g.setColor(Color.BLACK);
-			BasicStroke w = new BasicStroke(wPen); 
-			g.setStroke(w);
-			if (location.hasOpenFile())
-				for (int i = 0; i < plan.getWalls().length; i++) {
-					g.drawLine((int) plan.getWall(i).getX1() * m * bar, 
-							(int) plan.getWall(i).getY1() * m * bar, 
-							(int) plan.getWall(i).getX2() * m * bar, 
-							(int) plan.getWall(i).getY2() * m * bar);
-				}
+			drawWalls(g);
 
 			//отрисовываем внешний контур
-			Color green = new Color((float) 0.0, (float) 1.0, (float) 0.0, (float) 0.5);
-			g.setColor(green);
-			BasicStroke brd = new BasicStroke(borderPen); 
-			g.setStroke(brd);
-			if (location.hasOpenFile()) {
-				Border border = plan.getBorder();
-				for (int i = 0; i < border.getDotes().length - 1; i++) {
-					g.drawLine(border.getDote(i)[0] * m * bar, 
-					border.getDote(i)[1] * m * bar, 
-					border.getDote(i + 1)[0] * m * bar, 
-					border.getDote(i + 1)[1] * m * bar);
-				}
-			}
-	
-			//отрисовываем начало разбиения
-			/*g.setStroke(b);
-			g.setColor(Color.RED);
-			if (location.hasOpenFile()) {
-				for (int i = 0; i < location.hDotesNum; i++)
-					g.drawLine(location.hDotes.get(i) * m * bar, 0, 
-					location.hDotes.get(i) * m * bar, height);
-				for (int i = 0; i < location.vDotesNum; i++)
-					g.drawLine(0, location.vDotes.get(i) * m * bar, 
-					width, location.vDotes.get(i) * m * bar);
-			}*/
-		
-
-			//отрисовываем финальные фреймы
-			/*if (location.hasOpenFile()) {
-				g.setColor(Color.BLUE);
-				//g.setStroke(brd);
-				for (int i = 0; i < location.finalFramesNum; i++)
-					g.drawRect(location.finalFrames[i].getX1() * m * bar + 5,
-						location.finalFrames[i].getY1() * m * bar + 5,
-					((location.finalFrames[i].getX2() - location.finalFrames[i].getX1()) * m * bar) - 10,
-	 				((location.finalFrames[i].getY2() - location.finalFrames[i].getY1()) * m * bar) - 10);
-			}*/
+			drawBorder(g);
 			
 			//отрисовываем конечные ячейки
-			if (location.hasOpenFile()) {
-				g.setStroke(b);
-				g.setColor(Color.red);
-				Tail[] t = location.getTails();
-				for (int i = 0; i < location.getTailsNum(); i++)
-					g.drawRect((int) t[i].getX1() * m * bar,
-							(int) t[i].getY1() * m * bar,
-							(int) (t[i].getX2() - t[i].getX1()) * m * bar,
-							(int) (t[i].getY2() - t[i].getY1()) * m * bar);
-			}
+			drawTails(g);
+			
+			//отрисовываем базовые станции
+			drawStations(g);
  	}
+    	
+    public void drawInnerFrames(Graphics2D g) {
+    	if (location.hasOpenFile()) {
+    		Frame[][] frames = location.getStartFrames();
+			for (int i = 0; i < frames.length; i++)
+				for (int j = 0; j < frames[0].length; j++)
+					if (!frames[i][j].isUsed())
+						fillColor(frames[i][j].getX1() * m * bar, 
+						frames[i][j].getY1() * m * bar,
+						frames[i][j].getX2() * m * bar, 
+						frames[i][j].getY2() * m * bar);
+    	}
+    }
+    	
+    public void drawBaseLines(Graphics2D g) {
+       	
+    	BasicStroke b = new BasicStroke(bPen); 
+		g.setStroke(b);
+		g.setColor(Color.GRAY);
+		for (int i = 0; i < vBars; i++)
+			g.drawLine(0, bar * i, width, bar * i);
+		for (int i = 0; i < hBars; i++)
+			g.drawLine(bar * i, 0, bar * i, height);
+   }
+    
+    /*public void drawHelpLines(Graphics2D g) {
+    	BasicStroke b = new BasicStroke(bPen);
+    	g.setStroke(b);
+		g.setColor(Color.RED);
+		if (location.hasOpenFile()) {
+			for (int i = 0; i < location.hDotesNum; i++)
+				g.drawLine(location.hDotes.get(i) * m * bar, 0, 
+				location.hDotes.get(i) * m * bar, height);
+			for (int i = 0; i < location.vDotesNum; i++)
+				g.drawLine(0, location.vDotes.get(i) * m * bar, 
+				width, location.vDotes.get(i) * m * bar);
+    }*/
+    
+    public void drawBorder(Graphics2D g) {
+    	
+    	Color green = new Color((float) 0.0, (float) 1.0, (float) 0.0, (float) 0.5);
+		g.setColor(green);
+		BasicStroke brd = new BasicStroke(borderPen); 
+		g.setStroke(brd);
+		if (location.hasOpenFile()) {
+			Border border = plan.getBorder();
+			for (int i = 0; i < border.getDotes().length - 1; i++) {
+				g.drawLine(border.getDote(i)[0] * m * bar, 
+				border.getDote(i)[1] * m * bar, 
+				border.getDote(i + 1)[0] * m * bar, 
+				border.getDote(i + 1)[1] * m * bar);
+			}
+		}
+    }
 
-    	/** 
-     	* Заполняет цветом прямоугольник.
-     	*/
-	public void fillColor(int x1, int y1, int x2, int y2) {
+    public void drawTails(Graphics2D g) {
+    	
+    	if (location.hasOpenFile()) {
+    		BasicStroke b = new BasicStroke(bPen); 
+			g.setStroke(b);
+			g.setColor(Color.red);
+			Tail[] t = location.getTails();
+			for (int i = 0; i < location.getTailsNum(); i++)
+				g.drawRect((int) t[i].getX1() * m * bar,
+						(int) t[i].getY1() * m * bar,
+						(int) (t[i].getX2() - t[i].getX1()) * m * bar,
+						(int) (t[i].getY2() - t[i].getY1()) * m * bar);
+		}
+    }
+    
+    void drawWalls(Graphics2D g) {
+    	
+    	g.setColor(Color.BLACK);
+		BasicStroke w = new BasicStroke(wPen); 
+		g.setStroke(w);
+		if (location.hasOpenFile())
+			for (int i = 0; i < plan.getWalls().length; i++) {
+				g.drawLine((int) plan.getWall(i).getX1() * m * bar, 
+						(int) plan.getWall(i).getY1() * m * bar, 
+						(int) plan.getWall(i).getX2() * m * bar, 
+						(int) plan.getWall(i).getY2() * m * bar);
+			}
+    }
+    
+    /*public void drawFinalFrames(Graphics2D g) {
+    	
+    	if (location.hasOpenFile()) {
+			g.setColor(Color.BLUE);
+			//g.setStroke(brd);
+			for (int i = 0; i < location.finalFramesNum; i++)
+				g.drawRect(location.finalFrames[i].getX1() * m * bar + 5,
+					location.finalFrames[i].getY1() * m * bar + 5,
+				((location.finalFrames[i].getX2() - location.finalFrames[i].getX1()) * m * bar) - 10,
+ 				((location.finalFrames[i].getY2() - location.finalFrames[i].getY1()) * m * bar) - 10);
+    }*/
+    	
+    public void drawStations(Graphics2D g) {
+    	
+		if (location.hasOpenFile()) {
+			BasicStroke b = new BasicStroke(bPen); 
+			g.setStroke(b);
+			g.setColor(Color.BLUE);
+			for (int i = 0; i < plan.getStations().size(); i++)
+				g.drawOval((int) (plan.getStation(i).getX() * m * bar - rad * bar), 
+						(int) (plan.getStation(i).getY() * m * bar - rad * bar), 
+						(int) (rad * bar * 2), (int) (rad * bar * 2));
+		}
+    }
+
+    /** 
+    * Заполняет цветом прямоугольник.
+    */
+	public void fillColor(double d, double e, double f, double h) {
 		Color c = new Color((float) 0.0, (float) 0.0, (float) 0.1, (float) 0.5);
 		Graphics2D g = (Graphics2D) this.getGraphics();
 		g.setColor(c);
-		g.fillRect(x1, y1, x2 - x1, y2 - y1);
+		g.fillRect((int) d, (int) e, (int) (f - d), (int) (h - e));
 	}
 }
